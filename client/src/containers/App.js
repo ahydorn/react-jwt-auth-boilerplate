38c2e24ca0601d6083dcc0b432aad3ce258cffda
ahydorn <adamhydorn@gmail.com>
2019-07-29 12:36:06 -0700

    create state component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
+import Stuff from './Stuff';
 
 class App extends Component {
+  state = {
+    myFaveNumber: 0
+  };
   render() {
     return (
       <div>
         <Counter />
+        <Stuff />
       </div>
     );
   }
--- client/src/containers/Counter.js
+++ client/src/containers/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from './../components/Button/CounterButton';
+import Button from './../components/Buttons/CounterButton';
 
 class Counter extends Component {
   state = {
