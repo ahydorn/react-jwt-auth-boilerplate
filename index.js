a710d3e0ce343315f9c55d99c51c6f78b2231e0a
ahydorn <adamhydorn@gmail.com>
2019-07-29 10:12:15 -0700

    bookmark 7/29

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Deendencies
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Start express server
 const app = express();
 
 // Database setup
-mongoose.connect("mongodb://localhost:auth/auth", {
+mongoose.connect('mongodb://localhost:auth/auth', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
 // Middleware setup
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // If we ar ein production serve our clients build folder
 // This folder is created during production only\
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Routes setup
-const routes = require("./routes");
+const routes = require('./routes');
 app.use(routes);
 
 // Port assignemtn
